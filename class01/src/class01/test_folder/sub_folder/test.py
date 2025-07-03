c371113e568d98dec159e6c987618a230f29060f
Areej <areejzaheer96@gmail.com>
2025-07-04 02:04:08 +0500

    first_commit

---
+++ class01/src/class01/test_folder/sub_folder/test.py
@@ -0,0 +1,7 @@
+def Test():
+    print("This is a test function in a subfolder.")
+    print("It is used to demonstrate the structure of the project.")
+    print("You can run this function to see how it works.")
+    print("Make sure to import it correctly in your main script.")
+    if __name__ == "__main__":
+        Test()
